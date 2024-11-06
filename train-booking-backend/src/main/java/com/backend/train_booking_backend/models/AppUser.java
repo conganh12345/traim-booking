b0a10af3d2b24f9d69a2ff72fe11229f4023e1c3
Quốc Bảo <huuquocbao0903@gmail.com>
2024-11-06 12:05:44 +0700

    update  app user

--- train-booking-backend/src/main/java/com/backend/train_booking_backend/models/AppUser.java
+++ train-booking-backend/src/main/java/com/backend/train_booking_backend/models/AppUser.java
@@ -26,14 +26,6 @@ public class AppUser {
     @Column
     private String username;
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
     @Column
     private String email;
 
@@ -70,13 +62,21 @@ public class AppUser {
         this.id = id;
     }
 
-    // public String getEmail() {
-    // return email;
-    // }
-    //
-    // public void setEmail(String email) {
-    // this.email = email;
-    // }
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
     public String getPassword() {
         return password;
